6d8c894cd0da1f42b6dbd7920821cafef2544a07
Natraj Kaushik <natraj.iitb@gmail.com>
2013-04-21 20:11:50 -0400

    Fixed indentation in Utils.java

--- BluetoothReceiver/src/com/ninja/bluetooth/Utils.java
+++ BluetoothReceiver/src/com/ninja/bluetooth/Utils.java
@@ -1,26 +1,23 @@
 package com.ninja.bluetooth;
 
-
 public class Utils {
-	
+
 	/**
 	 * @param data byte array of data read from Arduino
 	 * @return message sent by the user
 	 */
-	public static String parseArduinoMessage(byte[] data){
+	public static String parseArduinoMessage(byte[] data) {
 		String _data = new String(data);
-		
+
 		String message;
-        if(_data.contains(Constants.EMERGENCY_MESSAGE)){
-        	message = Constants.EMERGENCY_MESSAGE;
-        }
-        else if(_data.contains(Constants.CANCEL_MESSAGE)){
-        	message = Constants.CANCEL_MESSAGE;
-        }
-        else{
-        	message = Constants.ERROR_MESSAGE;
-        }
-        
-        return message;
+		if (_data.contains(Constants.EMERGENCY_MESSAGE)) {
+			message = Constants.EMERGENCY_MESSAGE;
+		} else if (_data.contains(Constants.CANCEL_MESSAGE)) {
+			message = Constants.CANCEL_MESSAGE;
+		} else {
+			message = Constants.ERROR_MESSAGE;
+		}
+
+		return message;
 	}
 }
